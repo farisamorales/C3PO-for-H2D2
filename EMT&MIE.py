52d53e8918b496323ad8eba2c03a246cb77d8a0e
CWKing <codywillking@gmail.com>
2017-01-20 16:53:26 -0800

    Update EMT&MIE.py
    Small modifications. Noticed I was squaring some values in the EMT implementation in some places but not others; unfortunately hasn't changed erroneous results. Still figuring that out.

--- EMT&MIE.py
+++ EMT&MIE.py
@@ -7,15 +7,59 @@
     TODO:
     	-Impliment MIE theory
 	-Change to usage of pickle
-	-Consider other stylistic changes as per suggestion
+	-Generalize variable names to accomodate other substances?
+	-Make stylistic changes as per suggestion
 '''
 
-#import matplotlib.pyplot as plt
 from numpy import interp, real, imag
 #interp(x-coord of desired interpolates, x-coord of input data, y-coord of input data)
 #Returns a numpy array object; this can be quickly converted to a standard python list
+#I don't know if it's necessary (probably not) but I do it anyway just in case
 from kmod import whiteSpaceParser, getStart, getCol, listifier, columnizer
 from cmath import sqrt
+
+#--------------------
+#- Global Variables -
+#--------------------
+#For whether or not we will be making our own polluted matrix prior to re-inclusion; 
+#if true we do not make our own
+preDMat = False
+
+#For whether or not we will be writing new lists to memory. If True, we will
+#write to memory the results of both interpolations and EMT
+WRITE = False
+
+#For whether or not we will be generating graphs of the results of the intpolations
+#and of EMT. If true, we graph these results as functions of the wavelength
+GRAPH = True
+
+#---------------------------------------------
+#- File directories for the necessary tables -
+#---------------------------------------------
+
+#File directory for written tables
+directory = r'C:\Users\Cody\Desktop\School Documents\Physics\Independent Study\DielectricEffect'
+
+if not preDMat:
+    #Amorphous Carbon Optical Constants
+    dir_AC = directory + r'\AmorphCarbOptConst.dat'
+    
+    #Water Optical Constants
+    dir_WA = directory + r'\waterOpticalConstants.dat'
+
+else:
+   #Dirty Ice Optical Constants
+    dir_DI = directory + r'\DirtyIceOptConst.dat' 
+    
+#AstroSil Optical Constants
+dir_AS = directory+ r'\AstroSilOptConst.dat'
+
+#Desired Wavelengths
+dir_W = directory + r'\wav_set.dat'
+
+#Desired Grain Sizes; current table is currently unreadable by my modules so it needs to manually
+#be modified; sorry!
+dir_G = directory + r'\grain_size_grid_suvsil.list'
 
 #------------------------------------------------------
 #- Short functions which simplify (maybe) some things -
@@ -30,7 +74,7 @@ def trimmer(LIST, mini, maxi):
     return tempList
 #End trimming function
 #-------------------------------------------------------------
-#Short function to write to a file data from a dicitonary
+#Short function to write to a file data from a dictionary
 #Python doesn't care if we want the string read-in as a raw string
 #if it sees a \' at the end of a string it complains about end-of-line errors!
 def writer(DICT, FILENAME):
@@ -64,63 +108,72 @@ def imaginator(LIST, demaginate=False):
 #V: Ratio of pollutant volume to total volume
 def EMT(NM, KM, NI, KI, V=.5):
     M, I = NM + KM, NI + KI
-    F = (I ** 2 - M ** 2) / (I + 2.0 * M)
+    F = (I - M) / (I + 2.0 * M)
     AMOC = sqrt(M * (1 + ((3.0 * V * F) / (1 - V * F))))
     return float(real(AMOC)), float(imag(AMOC))
-#End short EMT function... it's very small
-#---------------------------------------------
-#- File directories for the necessary tables -
-#---------------------------------------------
-
-#File directory for wirtten tables
-directory = r'C:\Users\Cody\Desktop\School Documents\Physics\Independent Study\DielectricEffect'
-
-#Amorphous Carbon Optical Constants
-dir_AC = directory + r'\AmorphCarbOptConst.dat'
-
-#AstroSil Optical Constants
-dir_AS = directory+ r'\AstroSilOptConst.dat'
-
-#Dirty Ice Optical Constants
-dir_DI = directory + r'\DirtyIceOptConst.dat'
-
-#Water Optical Constants
-dir_WA = directory + r'\waterOpticalConstants.dat'
-
-#Desired Wavelengths
-dir_W = directory + r'\wav_set.dat'
-
-#Desired Grain Sizes; current table is currently unreadable by my modules so it needs to manually
-#be modified; sorry!
-dir_G = directory + r'\grain_size_grid_suvsil.list'
+#End short EMT function... yes this is really the extent of it.
 
 #-----------------------------------------------
 #- Writing data from tables in files to memory -
 #-----------------------------------------------
-#--------------------
-#- Amorphous Carbon -
-#--------------------
 
-#Open the Amorphous Carbon table in binary mode
-AC_Tab = open(dir_AC, 'rb')
+if not preDMat:
+    #--------------------
+    #- Amorphous Carbon -
+    #--------------------
+    #Open the Amorphous Carbon table in binary mode
+    AC_Tab = open(dir_AC, 'rb')
+    
+    #Start the reader at the appropriate postion
+    AC_Tab.seek(getStart(AC_Tab))
 
-#Start the reader at the appropriate postion
-AC_Tab.seek(getStart(AC_Tab))
+    #Do the thing! Seperate the values!
+    #Assign to mList the list of lists returned from Columnizer which took a single
+    #list returned by listifier (as well as the number of columns in the table
+    #found using the getCol module) which itself took a tuple returned by
+    #whiteSpaceParser which consisted of the modified contents of the read file
+    #in addition to the index location of whitespaces in that file.
+    #For more information, check kmod.py
+    mList = columnizer(listifier(whiteSpaceParser(AC_Tab.read())), getCol(AC_Tab))
+    AmorphCarb = {'WAV':mList[0], 'N':mList[1], 'K':mList[2]}
 
-#Do the thing! Seperate the values!
-#Assign to mList the list of lists returned from Columnizer which took a single
-#list returned by listifier (as well as the number of columns in the table
-#found using the getCol module) which itself took a tuple returned by
-#whiteSpaceParser which consisted of the modified contents of the read file
-#in addition to the index location of whitespaces in that file.
-#For more information, check kmod.py
-mList = columnizer(listifier(whiteSpaceParser(AC_Tab.read())), getCol(AC_Tab))
-AmorphCarb = {'WAV':mList[0], 'N':mList[1], 'K':mList[2]}
+    #Close the file and delete variables no longer needed
+    AC_Tab.close()
+    del(AC_Tab, mList, dir_AC)
+    
+    #-------------
+    #- Water Ice -
+    #-------------
+    #Similar proceedure for Water
+    WA_Tab = open(dir_WA, 'rb')
+    
+    WA_Tab.seek(getStart(WA_Tab))
+    
+    mList = columnizer(listifier(whiteSpaceParser(WA_Tab.read())), getCol(WA_Tab))
+    
+    Water = {'WAV':mList[0], 'N':mList[1], 'K':mList[2]}
 
-#Close the file and delete variables no longer needed
-AC_Tab.close()
-del(AC_Tab, mList, dir_AC)
+    WA_Tab.close()
 
+    del(WA_Tab, mList, dir_WA)
+
+else:
+    #-------------
+    #- Dirty Ice -
+    #-------------
+    #Similar proceedure for Dirty Ice
+    DI_Tab = open(dir_DI, 'rb')
+    
+    DI_Tab.seek(getStart(DI_Tab))
+
+    mList = columnizer(listifier(whiteSpaceParser(DI_Tab.read())), getCol(DI_Tab))
+
+    DirtyIce = {'WAV':mList[0], 'N':mList[1], 'K':mList[2]}
+
+    DI_Tab.close()
+
+    del(DI_Tab, mList, dir_DI)
+    
 #------------
 #- AstroSil -
 #------------
@@ -148,38 +201,6 @@ for i, ele in enumerate(AstroSil['N']):
 AS_Tab.close()
 
 del(AS_Tab, mList, dir_AS, i, ele)
-
-#-------------
-#- Dirty Ice -
-#-------------
-#Similar proceedure for Dirty Ice
-DI_Tab = open(dir_DI, 'rb')
-
-DI_Tab.seek(getStart(DI_Tab))
-
-mList = columnizer(listifier(whiteSpaceParser(DI_Tab.read())), getCol(DI_Tab))
-
-DirtyIce = {'WAV':mList[0], 'N':mList[1], 'K':mList[2]}
-
-DI_Tab.close()
-
-del(DI_Tab, mList, dir_DI)
-
-#---------
-#- Water -
-#---------
-#Similar proceedure for Water
-WA_Tab = open(dir_WA, 'rb')
-
-WA_Tab.seek(getStart(WA_Tab))
-
-mList = columnizer(listifier(whiteSpaceParser(WA_Tab.read())), getCol(WA_Tab))
-
-Water = {'WAV':mList[0], 'N':mList[1], 'K':mList[2]}
-
-WA_Tab.close()
-
-del(WA_Tab, mList, dir_WA)
 
 #-----------------
 #- Desired Waves -
@@ -216,10 +237,14 @@ del(G_Tab, mList, dir_G)
 #-----------------------------------------------------------
 #- Trimming Wave lists for useful wavelengths; the prequel - 
 #-----------------------------------------------------------
-#New wavelengths based on DirtyIce; determines wavelength for all others.
+#New wavelengths based on Primary Matrix Material; determines wavelength for all others.
 #As this is needed for interpolation it must be done before the interpolation
 #but assignment to other dictionaries must be done after
-Waves.update({'WAV':trimmer(Waves['WAV'], min(DirtyIce['WAV']), max(DirtyIce['WAV']))})
+if not preDMat:
+    Waves.update({'WAV':trimmer(Waves['WAV'], min(Water['WAV']), max(Water['WAV']))})
+
+else:
+    Waves.update({'WAV':trimmer(Waves['WAV'], min(DirtyIce['WAV']), max(DirtyIce['WAV']))})
 
 #---------------------------------------------------------------------------
 #- Interpolate using SciPy's (NumPy's?) interp function for missing values -
@@ -229,11 +254,26 @@ Waves.update({'WAV':trimmer(Waves['WAV'], min(DirtyIce['WAV']), max(DirtyIce['WA
 #I change these into complex numbers piecewise during EMT, I still can't figure
 #out why
 
-#Update Amorphous Carbon Dict with interpolates for reals
-AmorphCarb.update({'N':list(interp(Waves['WAV'], AmorphCarb['WAV'], AmorphCarb['N']))})
+if not preDMat:
+    #Update Amorphous Carbon Dict with interpolates for reals
+    AmorphCarb.update({'N':list(interp(Waves['WAV'], AmorphCarb['WAV'], AmorphCarb['N']))})
 
-#Update Amorphous Carbon Dict with interpolates for complex
-AmorphCarb.update({'K':imaginator(list(interp(Waves['WAV'], AmorphCarb['WAV'], AmorphCarb['K'])))})
+    #Update Amorphous Carbon Dict with interpolates for complex
+    AmorphCarb.update({'K':imaginator(list(interp(Waves['WAV'], AmorphCarb['WAV'], AmorphCarb['K'])))})
+
+    #Update Water Dict with interpolates for reals
+    Water.update({'N':list(interp(Waves['WAV'], Water['WAV'], Water['N']))})
+
+    #Update Water Dict with interpolates for complex
+    Water.update({'K':imaginator(list(interp(Waves['WAV'], Water['WAV'], Water['K'])))})
+
+else:
+    #Update DirtyIce Dict with interpolates for reals
+    DirtyIce.update({'N':list(interp(Waves['WAV'], DirtyIce['WAV'], DirtyIce['N']))})
+
+    #Update DirtyIce Dict with interpolates for complex
+    DirtyIce.update({'K':imaginator(list(interp(Waves['WAV'], DirtyIce['WAV'], DirtyIce['K'])))})
+
 
 #Update AstroSil Dict with interpolates for reals
 AstroSil.update({'N':list(interp(Waves['WAV'], AstroSil['WAV'], AstroSil['N']))})
@@ -241,33 +281,23 @@ AstroSil.update({'N':list(interp(Waves['WAV'], AstroSil['WAV'], AstroSil['N']))}
 #Update AstroSil Dict with interpolates for complex
 AstroSil.update({'K':imaginator(list(interp(Waves['WAV'], AstroSil['WAV'], AstroSil['K'])))})
 
-#Update DirtyIce Dict with interpolates for reals
-DirtyIce.update({'N':list(interp(Waves['WAV'], DirtyIce['WAV'], DirtyIce['N']))})
-
-#Update DirtyIce Dict with interpolates for complex
-#DirtyIce.update({'K':imaginator(list(interp(Waves['WAV'], DirtyIce['WAV'], DirtyIce['K']))}))
-DirtyIce.update({'K':list(interp(Waves['WAV'], DirtyIce['WAV'], DirtyIce['K']))})
-
-#Update Water Dict with interpolates for reals
-Water.update({'N':list(interp(Waves['WAV'], Water['WAV'], Water['N']))})
-
-#Update Water Dict with interpolates for complex
-Water.update({'K':imaginator(list(interp(Waves['WAV'], Water['WAV'], Water['K'])))})
-
 #----------------------------------------------------------
 #- Trimming Wave lists for useful wavelengths; the sequel - 
 #----------------------------------------------------------
-#New wavelengths for Amorphous Carbon
-AmorphCarb.update({'WAV':Waves['WAV']})
 
+if not preDMat:
+    #New wavelengths for Amorphous Carbon
+    AmorphCarb.update({'WAV':Waves['WAV']})
+
+    #New wavelengths for Water
+    Water.update({'WAV':Waves['WAV']})
+
+else:
+    #New wavelengths for DirtyIce
+    DirtyIce.update({'WAV':Waves['WAV']})
+    
 #New wavelengths for AstroSil
 AstroSil.update({'WAV':Waves['WAV']})
-
-#New wavelengths for Water
-Water.update({'WAV':Waves['WAV']})
-
-#New wavelengths for DirtyIce
-DirtyIce.update({'WAV':Waves['WAV']})
 
 #--------------------------------------------------------------
 #- Computing Optical Constants for Inclusion-Matrix Particles -
@@ -276,22 +306,36 @@ DirtyIce.update({'WAV':Waves['WAV']})
 #Initialize the Optical Constants dictionary
 IMPOptConst = {'WAV':Waves['WAV'], 'N':[], 'K':[]}
 
-#Iterate through the Water reals and utilize EMT to generate new
+#Iterate through designated reals and utilize EMT to generate new
 #optical constants
-for i, ele in enumerate(Water['N']):
+
+if not preDMat:
+    for i, ele in enumerate(Water['N']):
     
-    #Utilize EMT for Amorphous Carbon and Water
-    N, K = EMT(ele, Water['K'][i], AmorphCarb['N'][i], AmorphCarb['K'][i])
+        #Utilize EMT for Amorphous Carbon and Water
+        N, K = EMT(ele, Water['K'][i], AmorphCarb['N'][i], AmorphCarb['K'][i])
     
-    #Utilize the results from the last utilization of EMT for this run of EMT
-    #now with AstroSil
-    N, K = EMT(N, K, AstroSil['N'][i], AstroSil['K'][i])
+        #Utilize the results from the last utilization of EMT for this run of EMT
+        #now with AstroSil
+        N, K = EMT(N, K, AstroSil['N'][i], AstroSil['K'][i])
     
-    #Append the appropriate values to the IMP dictionary
-    IMPOptConst['N'].append(N)
-    if K < 0:
-        K = 1e-11
-    IMPOptConst['K'].append(K)
+        #Append the appropriate values to the IMP dictionary
+        IMPOptConst['N'].append(N)
+        if K < 0:
+            K = 1e-11
+        IMPOptConst['K'].append(K)
+            
+else:
+   for i, ele in enumerate(DirtyIce['N']):
+        
+        #Utilize EMT with the Predetermined Matrix and Inclusion
+        N, K = EMT(ele, DirtyIce['K'][i], AstroSil['N'][i], AstroSil['K'][i])
+        
+        #Append the appropriate values to the IMP dictionary
+        IMPOptConst['N'].append(N)
+        if K < 0:
+            K = 1e-11
+        IMPOptConst['K'].append(K) 
 
 del(i, ele, N, K)
 
@@ -299,41 +343,47 @@ del(i, ele, N, K)
 #- Modify complex coefficients to be real again -
 #------------------------------------------------
 #Must be done else MatPlotLib complains when attempting to plot the values
-imaginator(AmorphCarb['K'], True)
+if not preDMat:
+    imaginator(AmorphCarb['K'], True)
 
+    imaginator(Water['K'], True)
+
+else:
+    imaginator(DirtyIce['K'], True)
+    
 imaginator(AstroSil['K'], True)
 
-#imaginator(DirtyIce['K'], True)
-
-imaginator(Water['K'], True)
-
-WRITE = False
-
 if WRITE:
-    writer(AmorphCarb, 'AmorphCarbInterp.csv')
-    writer(AstroSil, 'AstroSilInterp.csv')
-    writer(DirtyIce, 'DirtyIceInterp.csv')
-    writer(Water, 'WaterInterp.csv')
-    writer(IMPOptConst, 'IMPOptConst.csv')
+    if not preDMat:
+        writer(AmorphCarb, 'AmorphCarbInterp2.csv')
+        writer(Water, 'WaterInterp2.csv')    
+    else:
+        writer(DirtyIce, 'DirtyIceInterp2.csv')
+    writer(AstroSil, 'AstroSilInterp2.csv')
+    writer(IMPOptConst, 'IMPOptConst2.csv')
     
-
-'''
-def flot(DICT, y):
-    if y == 'K':
-        plt.ylabel('Exitinction Coefficient')
-        plt.title('Extinction Coefficient Vs. Wavelength')
-        plt.yscale('log')
-    elif y == 'N':
-        plt.ylabel('Refractive Index')
-        plt.title('Refractive Index Vs. Wavelength')
-    plt.plot(DICT['WAV'], DICT[y])
-    plt.xscale('log')
-    plt.xlabel('Wavelength (microns)')    
-    plt.grid(True)
-    plt.show()
-flot(Water, 'K')
-flot(IMPOptConst, 'K')
-flot(AstroSil, 'K')
-flot(AmorphCarb, 'K')
-flot(DirtyIce, 'K')
-'''
+if GRAPH:
+    import matplotlib.pyplot as plt
+    def flot(DICT, y):
+        if y == 'K':
+            plt.ylabel('Exitinction Coefficient')
+            plt.title('Extinction Coefficient Vs. Wavelength')
+            plt.yscale('log')
+        elif y == 'N':
+            plt.ylabel('Refractive Index')
+            plt.title('Refractive Index Vs. Wavelength')
+            plt.plot(DICT['WAV'], DICT[y])
+            plt.xscale('log')
+            plt.xlabel('Wavelength (microns)')    
+            plt.grid(True)
+            plt.show()
+    
+    if not preDMat:          
+        flot(Water, 'N')
+        flot(AmorphCarb, 'N')
+    else:
+       flot(DirtyIce, 'N') 
+    flot(IMPOptConst, 'N')
+    flot(AstroSil, 'N')
+    
+    
