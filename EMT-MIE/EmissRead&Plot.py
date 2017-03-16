a2510c0eaaba463d2de3037205492841b10b55cd
CWKing <codywillking@gmail.com>
2017-03-16 12:24:39 -0700

    Update EmissRead&Plot.py

--- EMT-MIE/EmissRead&Plot.py
+++ EMT-MIE/EmissRead&Plot.py
@@ -1,29 +1,38 @@
-from kmod import listifier, whiteSpaceParser
-import matplotlib.pyplot as plt
-from math import log10
+try:
+	from kmod import listifier, whiteSpaceParser
+	import matplotlib.pyplot as plt
+	from math import log10
+except ImportError:
+	print('One or more required modules could not be found:\n\tkmod.listifier, kmod.whiteSpaceParser\n\tmatplotlib.pyplot\n\tmath.log10')
+	raw_input('Press any key to exit')
+	exit()
 
-DEFAULTDIR = r'C:\Users\Cody\Desktop\School Documents\Physics\Independent Study\DielectricEffect\testfiles\\'
+DEFAULTDIR = r'C:\Users\Cody\Desktop\School Documents\Physics\Independent Study\DielectricEffect\testfiles'
 print('Current default directory: ' + DEFAULTDIR)
-directory = raw_input('Please enter file directory: ')
+directory = raw_input('Enter the file directory (press Enter for default): ')
 
-fileName = raw_input('Please enter the designator (e.x. DI;AS;50-50); type HELP for help: ')
+fileName = raw_input('Enter the designator (e.x. DI;AS;50-50); type HELP for help: ')
 
 if directory == '':
 	directory = DEFAULTDIR
 
 if fileName.lower() == 'help':
-	print('Designators take the form MATRIX;INCLUSION;VOL_MATRIX-VOL_INCLUSION\nWhere MATRIX or INCLUSION is the two letter symbol for\nthe matrix/inclusion substance (e.x. DI for DirtyIce)\nVOL_MATRIX is a number between 0 and 100\nAndVOL_INCLUSION is 100 - VOL_MATRIX')
-	fileName = raw_input('Please enter the designator; type HELP for more help: ')
+	print('Designators take the form MATRIX[;INCLUSION;VOL_MATRIX-VOL_INCLUSION]\nWhere MATRIX or INCLUSION is the two letter symbol for\nthe matrix/inclusion substance (e.x. DI for DirtyIce)\nVOL_MATRIX is a number between 0 and 100\nAnd VOL_INCLUSION is 100 - VOL_MATRIX')
+	fileName = raw_input('Enter the designator; type HELP for more help: ')
 	if fileName.lower() == 'help':
 		print('Current substances and there associated symbols:\n\tDirtyIce - DI\n\tWaterIce - WA\n\tAmorphousCarbon - AC\n\tAstroSil - AS')
-		fileName = raw_input('Please enter the designator; don\'t type HELP for even more help: ')
+		fileName = raw_input('Enter the designator; don\'t type HELP for even more help: ')
 		if fileName.lower() == 'help':
-			print('Excuse me, What did I just tell you about asking for more help?')
-			raw_input('I\'m done. Press any key to exit')
+			print('You are beyond help')
+			raw_input('Press any key to exit')
 			exit()
 
-ffile = open(directory + 'Emissivities[' + fileName + '].csv', 'rb')
-
+try:
+	ffile = open(directory + '\Emissivities[' + fileName + '].csv', 'rb')
+except IOError:
+	print(directory + '\Emissivities[' + fileName + '].csv' + ' does not exist')
+	raw_input('Press any key to exit')
+	exit()
 def readEmiss(FILE):
 	FILE.seek(0)
 	Lamda = listifier(whiteSpaceParser(FILE.readline()))
